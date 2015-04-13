9e7e3ac71461a1404d441e6031f92e5a858f3a54
danielpevangelista <danielevangelista@computer-5.home>
2015-04-12 21:23:29 -0300

    Some fix.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,56 @@
 $(document).ready(function(){
-	console.info('entrou');
+	$('#retorno').fadeOut(100);
+
 	$('#calcularGrupo').on('submit',function(event){
 		event.preventDefault();
+		$('#retorno').fadeOut(100);
+		$('#verify').html('Verificando OVNI...');
+		$('#verify').prop( "disabled", true );
 		$.ajax({
-			method: "POST",
-			url: "./assets/php/process.php",
+			method : 'POST',
+			url : './assets/php/process.php',
+			dataType : 'JSON',
 			data: $(this).serialize()
 		}).done(function( data ) {
 			$('#retorno').fadeIn(300);
+			$('#verify').html('Verificar se o grupo será levado');
+			$('#verify').prop( "disabled", false );
+			processData(data);
+			console.info(data);
 		});
 	});
+
+	var processData = function(data) {
+		$('#resultCometa').html('');
+		$('#resultCometaValue').html('');
+		$('#resultCometaCalc').html('');
+		$('#resultCometaMod').html('');
+
+		$('#resultGrupo').html('');
+		$('#resultGrupoValue').html('');
+		$('#resultGrupoCalc').html('');
+		$('#resultGrupoMod').html('');
+
+		for ( var i = 0; i < data['cometa']['letters'].length; i++ ) {
+			$('#resultCometa').append('<td>' + data['cometa']['letters'][i] + '</td>');
+			$('#resultCometaValue').append('<td>' + data['cometa']['lettersValue'][i] + '</td>');
+		}
+		$('#resultCometaCalc').html(data['cometa']['value']);
+		$('#resultCometaMod').html(data['cometa']['mod']);
+
+		for ( var i = 0; i < data['grupo']['letters'].length; i++ ) {
+			$('#resultGrupo').append('<td>' + data['grupo']['letters'][i] + '</td>');
+			$('#resultGrupoValue').append('<td>' + data['grupo']['lettersValue'][i] + '</td>');
+		}
+		$('#resultGrupoCalc').append(data['grupo']['value']);
+		$('#resultGrupoMod').html(data['grupo']['mod']);
+
+		$('#isEquals').removeClass('btn-warning');
+		$('#isEquals').html('UFA!! O OVNI não vai pegar seu grupo.');
+		if ( data['cometa']['mod'] === data['grupo']['mod'] ) {
+			$('#isEquals').addClass('btn-warning');
+			$('#isEquals').html('Seu grupo VAI ser pego!!! Corram!!!');
+		}
+	};
+
 });
--- assets/php/process.php
+++ assets/php/process.php
@@ -1,7 +1,36 @@
 <?php
-	$grupo = ord(strtolower(htmlentities($_POST['grupo'])));
-	$cometa = ord(strtolower(htmlentities($_POST['cometa'])));
-	
-	function compare(){
+	$compare = array(
+		'cometa' => str_split( strtolower( htmlentities( $_POST['cometa'] ) ) ),
+		'grupo' => str_split( strtolower( htmlentities( $_POST['grupo'] ) ) )
+	);
+
+	function compare( $lettersArray ){
+		
+		$resultHtml = array();
+		$resultValue = 1;
+
+		foreach ($lettersArray as $letter) {
+			$letterValue = ( ord($letter) - 96 );
+
+			array_push($resultHtml, $letterValue);
+			$resultValue *= $letterValue;
+		}
+
+		$resultArray = array(
+			'letters'  => $lettersArray,
+			'lettersValue'  => $resultHtml,
+			'value' => $resultValue,
+			'mod'   => $resultValue % 45
+		);
+
+		return $resultArray;
 	};
+
+	$return = array(
+		'cometa' => compare($compare['cometa']),
+		'grupo'  => compare($compare['grupo']),
+		'isEquals' => $isEquals
+	);
+
+	echo json_encode($return);
 ?>
